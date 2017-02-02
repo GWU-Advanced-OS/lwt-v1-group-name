5c3e18eb7ce6c9683833100a3603891f37b2069f
huangdongawm <huangdongawm2016@gmail.com>
2017-02-01 23:14:45 -0500

    head file

---
+++ lwt.h
@@ -0,0 +1,48 @@
+#ifndef _LWT_H
+#define _LWT_H
+
+#define STACK_SIZE 4096
+
+#define LWT_INFO_NTHD_RUNNABLE 0x1;
+#define LWT_INFO_NTHD_ZOMBIES 0x2;
+#define LWT_INFO_NTHD_BLOCKED 0x3;
+
+typedef unsigned long ulong;
+typedef unsigned int uint;
+typedef unsigned short ushort;
+typedef unsigned char uchar;
+
+typedef void *(*lwt_fn_t) (void *);
+typedef int lwt_info_t
+typedef enum
+{
+	_ACTIVE,
+	_DEAD,
+	_BLOCKED,
+	_WAITING,
+}_TCB_STATE;
+
+typedef struct lwt_tcb
+{
+	int id;
+	ulong ip;
+	ulong sp;
+}lwt_tcb, *lwt_t;
+
+/*
+ * auto-increment counter for thread
+ */
+int lwt_counter;	
+
+lwt_info_t LWT_INFO_NTHD_RUNNABLE;
+lwt_info_t LWT_INFO_NTHD_ZOMBIES;
+lwt_info_t LWT_INFO_NTHD_BLOCKED;
+
+int lwt_id(lwt_t spe_tcb);
+lwt_t lwt_current(void);
+int lwt_yield(lwt_t destination);
+void *lwt_join(lwt_t thread);
+void lwt_die(void *data);
+lwt_t lwt_create(lwt_fn_t fn, void *data);
+int lwt_info(lwt_info_t t);
+#endif
